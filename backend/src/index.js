65772d606f2136e252d6784fa8380f52cefcf575
ranggadwias <ranggadwi783@gmail.com>
2025-08-18 10:56:27 +0700

    feat: initial setup for user authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const corsOption = {
 const app = express();
 app.use(express.json());
 app.use(cors(corsOption));
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ backend/src/models/User.js
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 8,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const User = model("User", userSchema);
+export default User;
