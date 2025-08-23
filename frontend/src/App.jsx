a706758f816aee2b7983383567f8278115c0a557
ranggadwias <ranggadwi783@gmail.com>
2025-08-23 11:54:32 +0700

    fix: correct routing and redirect logic on root path in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const token = localStorage.getItem("token");
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="login" replace />} />
+      <Route
+        path="/"
+        element={
+          token ? <Dashboard /> : <Navigate to="/login" replace={true} />
+        }
+      />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
     </Routes>
